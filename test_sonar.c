c756aa1e85301c7c6ef353fc973633d3cf0cae42
Derek Faust <faust.df@gmail.com>
2013-11-11 09:37:02 -0500

    Initial commit for sonar test program

---
+++ test_sonar.c
@@ -0,0 +1,33 @@
+/* test_sonar.h
+ * 
+ * Program to test the sonar module sonar.c
+ *
+ * MAE3780 Mechatronics Final Project 2013
+ * Cornell University
+ * Derek Faust, Nicole Panega, Abdullah Sayeem
+ */
+
+// Include standard libraries
+#include <avr/io.h>		// For general I/O
+#include <util/delay.h>	// For delay
+#include <stdint.h>		// For data types
+
+// Include local libraries
+#include "sonar.h"		// Library to be tested
+#include "serial.h"		// Library for serial terminal readout
+
+int main(void)
+{
+    sonar_init();		// Initialize Sonar
+	uint16_t distance; 	// Initialize distance variable
+	
+	// Loop forever
+	while(1){
+		// Get distance
+        distance = sonar_getDistance(0);
+		// Print the distance
+		printf("The measured distance is %u", distance)
+		// Wait a little
+		_delay_ms(100);
+    }
+}
