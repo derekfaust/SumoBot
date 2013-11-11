c224c4294ae9fb568b921160220268329dd2febb
Derek Faust <faust.df@gmail.com>
2013-11-10 22:54:27 -0500

    Added Atmel Studio auxiliary files

---
+++ SumoBot.c
@@ -0,0 +1,17 @@
+/*
+ * MechatronicsRobot.c
+ *
+ * Created: 11/10/2013 10:41:10 PM
+ *  Author: dfaust
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
