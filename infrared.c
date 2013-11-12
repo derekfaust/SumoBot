d7e22b75bac3a136324d2e776053fd0c09291d45
Derek Faust <faust.df@gmail.com>
2013-11-11 22:43:51 -0500

    Implemented IR sensor

---
+++ infrared.c
@@ -0,0 +1,69 @@
+/* infrared.c
+ * 
+ * Module contains everything needed to operate the infrared sensors
+ *
+ * MAE3780 Mechatronics Final Project 2013
+ * Cornell University
+ * Derek Faust, Nicole Panega, Abdullah Sayeem
+ */
+
+// Define Constants
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+#define PULSE_LENGTH 100	// Pulse length in # of periods
+#define IR_HALF_PERIOD 13	// Half the period in us
+
+//Include standard data types
+#include <stdint.h>			// For standard data types	
+#include <avr/io.h>			// For general I/O
+#include <util/delay.h>		// For delays
+
+/* Begin Internal Functions Here
+ * =============================
+ *
+ */
+
+uint8_t pollInfrared(void){
+// Poll all of the infrared sensors
+	
+	//Initialize map variable
+	uint8_t map;
+	
+	// Pulse the output port
+	uint8_t iterpulse; 			// Initialize iteration variable
+
+	// Loop for the number of periods in the pulse
+	for (iter_pulse = 0; iter_pulse<PULSE_LENGTH; iter_pulse++){
+		PORTC |= (1<<7);				// Turn on the LEDs
+		_delay_us (IR_HALF_PERIOD) ;	// Wait half the period
+		PORTC &= ~(1<<7);				// Turn off the LEDs
+		_delay_us (IR_HALF_PERIOD);		// Wait half the period
+	}
+	
+	// Record the received pulses from
+	// the first 6 registers in port c
+	map = PINC|(~((1<<6)|(1<<7))); 
+
+	// Return the results
+	return map;
+}
+
+/* Begin Global Functions Here
+ * ===========================
+ *
+ */
+
+void infrared_init(){
+// Function to initialize all IR sensors.
+	
+	DDRD |= (1<<7);				// Set PD7 to output
+	DDRC &= ~((1<<7)|(1<<6));	// Set PC0-5 to input
+}
+
+int8_t infrared_getMap(){
+// Function to get a map locating where
+// around the periphery object are detected.
+	uint8_t map = pollInfrared();
+	return map;
+}
