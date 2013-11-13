d52fd305187b376e7c363d5abc56eedeff570bad
Derek Faust <faust.df@gmail.com>
2013-11-13 13:47:21 -0500

    Changed infrared module to use multiple frequencies

--- infrared.c
+++ infrared.c
@@ -12,7 +12,7 @@
 #define F_CPU 16000000UL
 #endif
 #define PULSE_LENGTH 100	// Pulse length in # of periods
-#define IR_HALF_PERIOD 13	// Half the period in us
+#define NUM_ZONES 2			// Number of zones
 
 //Include standard libraries
 #include <stdint.h>			// For standard data types	
@@ -22,34 +22,48 @@
 // Include local modules
 #include "infrared.h"		// Include own header file
 
+// Define array of half-periods (closest range first)
+static uint8_t half_period[]={13,14};
+
 /* Begin Internal Functions Here
  * =============================
  *
  */
 
-uint8_t pollInfrared(void){
-// Poll all of the infrared sensors
+uint8_t pollInfrared(uint8_t zone){
+// Poll all of the infrared sensors in the given zone
 	
-	//Initialize map variable
-	uint8_t map;
-	
-	// Pulse the output port
-	uint8_t iter_pulse; 			// Initialize iteration variable
+	if (zone<NUM_ZONES){
 
-	// Loop for the number of periods in the pulse
-	for (iter_pulse = 0; iter_pulse<PULSE_LENGTH; iter_pulse++){
-		PORTC |= (1<<7);				// Turn on the LEDs
-		_delay_us (IR_HALF_PERIOD) ;	// Wait half the period
-		PORTC &= ~(1<<7);				// Turn off the LEDs
-		_delay_us (IR_HALF_PERIOD);		// Wait half the period
+		//Initialize map variable
+		uint8_t map;
+		
+		// Pulse the output port
+		uint8_t iter_pulse; 			// Initialize iteration variable
+		uint8_t iter_period;
+
+		// Loop for the number of periods in the pulse
+		for (iter_pulse = 0; iter_pulse<PULSE_LENGTH; iter_pulse++){
+			PORTC |= (1<<7);				// Turn on the LEDs
+			for (iter_period = 0; iter_period<half_period[zone]; iter_period++){
+				_delay_us (1);				// Wait half the period
+			}
+			PORTC &= ~(1<<7);				// Turn off the LEDs
+			for (iter_period = 0; iter_period<half_period[zone]; iter_period++){
+				_delay_us (1);				// Wait half the period
+			}
+		}
+		
+		// Record the received pulses from
+		// the first 6 registers in port c
+		map = PINC|(~((1<<6)|(1<<7))); 
+
+		// Return the results
+		return map;
 	}
-	
-	// Record the received pulses from
-	// the first 6 registers in port c
-	map = PINC|(~((1<<6)|(1<<7))); 
-
-	// Return the results
-	return map;
+	else{
+		return 0;
+	}
 }
 
 /* Begin Global Functions Here
@@ -67,6 +81,6 @@ void infrared_init(){
 int8_t infrared_getMap(){
 // Function to get a map locating where
 // around the periphery object are detected.
-	uint8_t map = pollInfrared();
+	uint8_t map = pollInfrared(0);
 	return map;
 }
