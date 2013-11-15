57ab6af8e480340924e855e0254d15ffadffec76
Derek Faust <faust.df@gmail.com>
2013-11-15 18:12:28 -0500

    Added partially-complete distance calculation to infrared module

--- infrared.c
+++ infrared.c
@@ -13,6 +13,8 @@
 #endif
 #define PULSE_LENGTH 100	// Pulse length in # of periods
 #define NUM_ZONES 2			// Number of zones
+#define NUM_IR 6			// Number of IR sensors
+#define SMOOTHING_FACTOR .7	// Smoothing factor
 
 //Include standard libraries
 #include <stdint.h>			// For standard data types	
@@ -24,20 +26,21 @@
 
 // Define array of half-periods (closest range first)
 static uint8_t half_period[]={13,6};
+static uint8_t zone_dist[]={12,6}
+static uint8_t bin_map[NUM_ZONES];
+static uint8_t ir_hist[NUM_ZONES][NUM_IR];
+static uint8_t distance[NUM_IR];
 
 /* Begin Internal Functions Here
  * =============================
  *
  */
 
-uint8_t pollInfrared(uint8_t zone){
+void pollInfrared(void){
 // Poll all of the infrared sensors in the given zone
-	
-	if (zone<NUM_ZONES){
+	uint8_t iter_zone;
+	for (iter_zone=0;iter_zone<NUM_ZONES; iter_zone++){
 
-		//Initialize map variable
-		uint8_t map;
-		
 		// Pulse the output port
 		uint8_t iter_pulse; 			// Initialize iteration variable
 		uint8_t iter_period;
@@ -45,24 +48,42 @@ uint8_t pollInfrared(uint8_t zone){
 		// Loop for the number of periods in the pulse
 		for (iter_pulse = 0; iter_pulse<PULSE_LENGTH; iter_pulse++){
 			PORTD |= (1<<7);				// Turn on the LEDs
-			for (iter_period = 0; iter_period<half_period[zone]; iter_period++){
+			for (iter_period = 0; iter_period<half_period[iter_zone]; iter_period++){
 				_delay_us (1);				// Wait half the period
 			}
 			PORTD &= ~(1<<7);				// Turn off the LEDs
-			for (iter_period = 0; iter_period<half_period[zone]; iter_period++){
+			for (iter_period = 0; iter_period<half_period[iter_zone]; iter_period++){
 				_delay_us (1);				// Wait half the period
 			}
 		}
 		
 		// Record the received pulses from
 		// the first 6 registers in port c
-		map = (~PINC)|0x3F;
-
-		// Return the results
-		return map;
+		bin_map[iter_zone] = (~PINC)|0x3F;
 	}
-	else{
-		return 0;
+}
+
+void calcDistance(void){
+	uint8_t i_zone;
+	uint8_t i_sensor;
+	float momentum;
+	uint8_t measurement;
+	for(i_zone=0;i_zone<NUM_ZONES;i_zone++){
+		for(i_sensor=0;i_sensor<NUM_IR;i_sensor++){
+			if ((bin_map[i_zone]>>i_sensor)&1){
+				measurement = zone_dist[i_zone]*(1-SMOOTHING_FACTOR);
+			}else{
+				measurement = 0;
+			}
+			momentum = ir_hist[i_zone][i_sensor]*SMOOTHING_FACTOR;
+			ir_hist[i_zone][i_sensor] = momentum + measurement;
+		}
+	}
+	for(i_sensor=0;i_sensor<NUM_IR;i_sensor++){
+		
+		if((ir_hist[NUM_ZONES-1]+(ir_hist[NUM_ZONES-1]>>3))>=zone_dist[NUM_ZONES-1]){
+			distance[i_sensor] = zone_dist[NUM_ZONES-1];
+		}
 	}
 }
 
