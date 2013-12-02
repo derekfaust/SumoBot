99c80ac93f0660395babcc92d460556c5aef7774
Derek Faust <faust.df@gmail.com>
2013-12-02 09:03:49 -0500

    Implemented main routine to call others

--- SumoBot.c
+++ SumoBot.c
@@ -7,12 +7,30 @@
  * Derek Faust, Nicole Panega, Abdullah Sayeem
  */
 
+// Include general I/O functionality
 #include <avr/io.h>
 
-int main(void)
-{
-    while(1)
-    {
-        //TODO:: Please write your application code 
+// Include custom modules that need to be initialized
+#include "routines.h"
+#include "indicator.h"
+#include "infrared.h"
+#include "motor.h"
+#include "qti.h"
+#include "sonar.h"
+
+// Main code loop that never exits
+int main(void){
+	
+	// Initialize modules
+	indicator_init();
+	infrared_init();
+	motor_init();
+	qti_init();
+	sonar_init();
+
+    while(1){
+		// Keep performing the search routine, which leads
+		// into other necessary routines.
+        routines_search();
     }
 }
